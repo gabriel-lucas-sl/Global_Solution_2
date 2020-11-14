0c3955217ca7267e1e4b8f7ac9083b19b9b60469
LincTech-00 <linc.tech.fiap@gmail.com>
2020-11-14 19:19:08 -0300

    Metodos nas classes DAO.

---
+++ src/br/com/linctech/beans/PresenteAlternativo.java
@@ -0,0 +1,21 @@
+package br.com.linctech.beans;
+
+import java.time.LocalDate;
+
+public class PresenteAlternativo extends Timeline{
+
+	public PresenteAlternativo() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public PresenteAlternativo(String acontecimentos, LocalDate data, String descricao, String ano,
+			Personagem personagem) {
+		super(acontecimentos, data, descricao, ano, personagem);
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+	
+}
